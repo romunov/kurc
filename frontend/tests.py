3da90b318901b4e67f6209edc429cf0898190dc4
Roman Luštrik <romunov@gmail.com>
2016-08-23 17:42:42 +0200

    migrations

--- frontend/tests.py
+++ frontend/tests.py
@@ -3,42 +3,42 @@ from django.test import Client
 
 
 class TestResponses(TestCase):
-	def setUp(self):
-		self.c = Client()
 
-	def test_index(self):
-		resp = self.c.get('/')
-		self.assertEqual(resp.status_code, 200)
+    def setUp(self):
+        self.c = Client()
 
-	def test_settings(self):
-		resp = self.c.get('/settings')
-		self.assertEqual(resp.status_code, 200)
+    def test_index(self):
+        resp = self.c.get('/')
+        self.assertEqual(resp.status_code, 200)
 
-	def test_docs(self):
-		resp = self.c.get('/docs')
-		self.assertEqual(resp.status_code, 200)
+    def test_settings(self):
+        resp = self.c.get('/settings')
+        self.assertEqual(resp.status_code, 200)
 
-	def test_logout(self):
-		resp = self.c.get('/logout')
-		self.assertEqual(resp.status_code, 200)
+    def test_docs(self):
+        resp = self.c.get('/docs')
+        self.assertEqual(resp.status_code, 200)
 
-	def test_login(self):
-		resp = self.c.get('/login')
-		self.assertEqual(resp.status_code, 200)
+    def test_logout(self):
+        resp = self.c.get('/logout')
+        self.assertEqual(resp.status_code, 200)
 
-	def test_welcome(self):
-		resp = self.c.get('/welcome')
-		self.assertEqual(resp.status_code, 200)
+    def test_login(self):
+        resp = self.c.get('/login')
+        self.assertEqual(resp.status_code, 200)
 
-	def test_stats(self):
-		resp = self.c.get('/stats')
-		self.assertEqual(resp.status_code, 200)
+    def test_welcome(self):
+        resp = self.c.get('/welcome')
+        self.assertEqual(resp.status_code, 200)
 
-	def test_upload(self):
-		resp = self.c.get('/upload')
-		self.assertEqual(resp.status_code, 200)
+    def test_stats(self):
+        resp = self.c.get('/stats')
+        self.assertEqual(resp.status_code, 200)
 
-	def test_view_file(self):
-		resp = self.c.get('/0000')
-		self.assertEqual(resp.status_code, 200)
+    def test_upload(self):
+        resp = self.c.get('/upload')
+        self.assertEqual(resp.status_code, 200)
 
+    def test_view_file(self):
+        resp = self.c.get('/0000')
+        self.assertEqual(resp.status_code, 200)
