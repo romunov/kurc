fb82bb9c729733e91ab10ca09841a0a21a3f3bd8
romunov <romunov@gmail.com>
2016-09-08 10:30:23 +0200

    added static_root

--- kurc/settings.py
+++ kurc/settings.py
@@ -104,6 +104,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/home/romunov/kurc/static'
 
 LOGIN_URL = '/login'
 LOGIN_REDIRECT_URL = '/docs'
