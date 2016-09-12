13ffa6b3a68137c274fb876cfa5aa0bc31bd5417
Roman Luštrik <romunov@gmail.com>
2016-09-12 07:57:52 +0200

    probam ujet oauth2callback

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,4 +1,3 @@
-from django.conf.urls import url
 from . import views
 from django.conf.urls import include, url
 
@@ -11,6 +10,5 @@ urlpatterns = [
     url(r'^welcome$', views.login, name="welcome"),
     url(r'^stats$', views.stats, name="stats"),
     url(r'^upload$', views.upload_file, name="upload_file"),
-    url(r'^(?P<doc_id>[0-9]{1,4}$)', views.view_file, name='view_file'),
-    url(r'^gmail/', views.auth_return, name='return'),
+    url(r'^(?P<doc_id>[0-9]{1,4}$)', views.view_file, name='view_file')
 ]
